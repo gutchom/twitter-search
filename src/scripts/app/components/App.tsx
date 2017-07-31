fd704c4142a55f71d8b03799b451436e5dc9565f
gutchom <yskz.tg@gmail.com>
2017-08-01 04:47:55 +0900

    Remove sample headding

--- src/scripts/app/components/App.tsx
+++ src/scripts/app/components/App.tsx
@@ -4,7 +4,6 @@ import LoggedInput from './LoggedInput/LoggedInput'
 const App: SFC<{}> = () => {
   return (
     <div className="app">
-      <h1>Hello, world!</h1>
       <LoggedInput name="query"/>
     </div>
   )
