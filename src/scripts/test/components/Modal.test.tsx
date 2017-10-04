40bf0648dfcbbab233e7e5ff345b4c2d72ed5aa7
gutchom <yskz.tg@gmail.com>
2017-10-04 11:15:01 +0900

    Add test

---
+++ src/scripts/test/components/Modal.test.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import * as assert from 'power-assert'
+import Enzyme, { mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import Modal from 'app/components/Modal'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+let isOpen = true
+function handleCancel() { isOpen = false }
+
+describe('Modal.tsx', function () {
+  it('has children component', function () {
+    const wrapper = mount(<Modal isOpen={isOpen} onCancel={handleCancel}><p>Sample text.</p><p>Sample text.</p></Modal>)
+
+    assert.strictEqual(wrapper.find('p').length, 2)
+  })
+
+  it('has header component', function () {
+    const wrapper = mount(
+      <Modal isOpen={isOpen}
+             onCancel={handleCancel}
+             header={<span>header</span>}>
+        <p>Sample text.</p></Modal>)
+
+    assert.strictEqual(wrapper.find('header').length, 1)
+  })
+
+  it('has footer component', function () {
+    const wrapper = mount(
+      <Modal isOpen={isOpen}
+             onCancel={handleCancel}
+             footer={<span>footer</span>}>
+        <p>Sample text.</p></Modal>)
+
+    assert.strictEqual(wrapper.find('footer').length, 1)
+  })
+})
