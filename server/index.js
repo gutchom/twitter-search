f0f33738e64680102c73d6790a446c406963fbaa
gutchom <yskz.tg@gmail.com>
2017-10-22 06:06:55 +0900

    Enhance mock server template

--- server/index.js
+++ server/index.js
@@ -3,16 +3,23 @@ const fs = require('fs')
 const path = require('path')
 const express = require('express')
 const app = express()
-
-const userInfo = JSON.stringify(require('./data/userInfo.json'))
+const account = JSON.stringify(require('./data/account.json'))
 
 app.use('/assets', express.static('public'))
 app.set('views', path.join(__dirname, './views'))
 app.set('view engine', 'pug')
 
-app.get('/', (req, res) => {
-  console.log('[server]: GET "/"'.cyan)
-  res.render('search.pug', { userInfo })
+app.use((req, res, next) => {
+  console.log(`[server]: ${req.method} ${req.path.cyan} from ${req.ip.cyan}`)
+  next()
+})
+
+app.route('/').get((req, res) => {
+  res.render(`top.pug`)
+})
+
+app.route('/:page').get((req, res) => {
+  res.render('app.pug', { name: req.params.page, account })
 })
 
 const server = app.listen(process.env.PORT || 3000, () => {
