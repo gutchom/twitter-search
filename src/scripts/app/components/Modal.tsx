da6118e7a64f8f6737c0b0e852d87aa0195635c1
gutchom <yskz.tg@gmail.com>
2017-10-27 01:02:06 +0900

    Reformat markup of Modal component

--- src/scripts/app/components/Modal.tsx
+++ src/scripts/app/components/Modal.tsx
@@ -85,19 +85,19 @@ export default class Modal extends React.Component<ModalProps, {}> {
 
   render() {
     return (
-      <div
-        ref={this.rootRef}
-        className={`modal ${this.props.className} ${this.props.visible ? 'modal--visible' : ''}`}
-      >
+      <div className={`modal ${this.props.className} ${this.props.visible ? 'modal--visible' : ''}`} ref={this.rootRef}>
         <div className="modal--window">
-          {this.props.header && <header>{this.props.header}</header>}
-          {this.props.footer && <footer>{this.props.footer}</footer>}
-          <div className="modal--content" ref={this.contentRef}>
-            {this.props.children}
-          </div>
           <button className="modal--close" onClick={this.handleCloseClick}>
             <i className="fa fa-times" aria-hidden="true" />
           </button>
+
+          {this.props.header && <header>{this.props.header}</header>}
+
+          <div className="modal--content" ref={this.contentRef}>
+            {this.props.children}
+          </div>
+
+          {this.props.footer && <footer>{this.props.footer}</footer>}
         </div>
       </div>
     )
